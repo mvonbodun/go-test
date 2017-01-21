1176fb3cf30cc82fcc9b88b2f4e6d7fb0aa7fbf6
Admin GeauxCommerce <admin@geauxcommerce.org>
2017-01-20 21:59:46 -0600

    first commit

---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mvonbodun/go-test/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println("This is vb2")
+	fmt.Println("Here is the 3rd line")
+}
